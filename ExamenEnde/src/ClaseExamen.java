9719cb2e169d52410eeacb8e4643225c8abc3d78
Balirina <47902373+Balirina@users.noreply.github.com>
2019-05-24 09:48:08 +0200

    Update ClaseExamen.java

--- ExamenEnde/src/ClaseExamen.java
+++ ExamenEnde/src/ClaseExamen.java
@@ -4,7 +4,7 @@ public class ClaseExamen {
 	private String jughfadgufldgf;
 	 public ClaseExamen()
 	 {
-		 
+		 dgfalugfalidgfdlauidfdaf
 	 }
 	 public static void main(String[] args) {
 		new ClaseExamen();
